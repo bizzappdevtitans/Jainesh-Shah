c3719ed886c6b7fdeb5153594d9074901dfb249f
Bizzappdev <BAD@bizzappdev.com>
2023-10-11 14:57:13 +0530

    [WIP][T00461]Added new docstring

--- validate_vendor/models/supplier_info.py
+++ validate_vendor/models/supplier_info.py
@@ -6,14 +6,15 @@ from odoo.exceptions import ValidationError
 class SupplierInfo(models.Model):
     _inherit = "product.supplierinfo"
 
-    @api.constrains("name")
+    @api.constrains(
+        "name", "min_qty", "date_start", "date_end", "product_tmpl_id")
     def _check_another_vendor_pricelist(self):
-        print("AAAA")
-        print(self.name.id)
-        print(self.min_qty)
-        print(self.date_start)
-        print(self.date_end)
-        print(self.product_tmpl_id.id)
+        """This function will check for if same pricelist is present with same vendor,
+        same quantity,same range of time and for same product then it will raise an
+        validation error
+        the code line id != self.id is written because while updating vendor pricelist
+        from page vendor in product template validation can occur
+        T00461"""
         record = self.env["product.supplierinfo"].search(
             [
                 ("name", "=", self.name.id),
@@ -24,6 +25,5 @@ class SupplierInfo(models.Model):
                 ("id", "!=", self.id),
             ]
         )
-        print(record)
         if record:
             raise ValidationError(_("Vendor Price List Already Present"))
