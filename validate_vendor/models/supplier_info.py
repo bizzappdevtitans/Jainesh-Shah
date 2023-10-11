c943741dd3b87dd0396ca6cca10d676d2541ae25
Bizzappdev <BAD@bizzappdev.com>
2023-10-11 15:10:20 +0530

    [WIP][T00461]Update a Validation message

--- validate_vendor/models/supplier_info.py
+++ validate_vendor/models/supplier_info.py
@@ -6,8 +6,7 @@ from odoo.exceptions import ValidationError
 class SupplierInfo(models.Model):
     _inherit = "product.supplierinfo"
 
-    @api.constrains(
-        "name", "min_qty", "date_start", "date_end", "product_tmpl_id")
+    @api.constrains("name", "min_qty", "date_start", "date_end", "product_tmpl_id")
     def _check_another_vendor_pricelist(self):
         """This function will check for if same pricelist is present with same vendor,
         same quantity,same range of time and for same product then it will raise an
@@ -26,4 +25,9 @@ class SupplierInfo(models.Model):
             ]
         )
         if record:
-            raise ValidationError(_("Vendor Price List Already Present"))
+            raise ValidationError(
+                _(
+                    "Vendor Price List Already Present with\n"
+                    + "Same Vendor\nSame Quantity\nSame Validate time range\nsame product"
+                )
+            )
